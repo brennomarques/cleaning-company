9bf084c59d375818fe84d1bd0c987ef90556e10f
brenno.lima <brenno.lima@soluti.com.br>
2021-12-11 19:39:03 -0300

    Trocado nome do input pesquisa

--- resources/views/simple/simple.blade.php
+++ resources/views/simple/simple.blade.php
@@ -57,8 +57,8 @@
 
                 </ul>
                 <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    <input class="form-control me-2" type="search" placeholder="Pesquisar algo" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Pesquisar</button>
                 </form>
             </div>
         </div>
